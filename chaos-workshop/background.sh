f505a2102d1aafa08e3b6b936a86b2f2160f51a4
Jason Yee <jason.yee@datadoghq.com>
2019-08-14 16:28:20 -0700

    pre-apply k8s yaml

--- chaos-workshop/background.sh
+++ chaos-workshop/background.sh
@@ -2,5 +2,15 @@
 mkdir /chaos-workshop
 git clone https://github.com/jyee/distributed-tracing-with-apm-workshop /chaos-workshop
 cd /chaos-workshop
-git fetch origin k8s-golang-workshop
-git checkout k8s-golang-workshop
+git fetch origin k8s-autodiscovery
+git checkout k8s-autodiscovery
+
+kubectl create secret generic postgres-user --from-literal=token=postgres
+kubectl create secret generic postgres-password --from-literal=token=postgres-password
+
+kubectl apply -f k8s-yaml-files/postgres-deploy.yaml
+kubectl apply -f k8s-yaml-files/redis-deploy.yaml
+kubectl apply -f k8s-yaml-files/pumps-service.yaml
+kubectl apply -f k8s-yaml-files/sensors-api.yaml
+kubectl apply -f k8s-yaml-files/node-api.yaml
+kubectl apply -f k8s-yaml-files/frontend-service.yaml
