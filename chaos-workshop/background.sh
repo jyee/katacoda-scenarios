735eb4a3aa30c3ac9d1e2ac1b2e3eba0ffe2d249
Jason Yee <jason.yee@datadoghq.com>
2019-08-14 15:56:44 -0700

    Need to fetch branch from remote before checkout

--- chaos-workshop/background.sh
+++ chaos-workshop/background.sh
@@ -2,4 +2,5 @@
 mkdir /chaos-workshop
 git clone https://github.com/jyee/distributed-tracing-with-apm-workshop /chaos-workshop
 cd /chaos-workshop
+git fetch origin k8s-golang-workshop
 git checkout k8s-golang-workshop
