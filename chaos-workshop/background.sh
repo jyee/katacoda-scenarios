f5ad03a43b4a80da4c4c9325816d4ac85cb23f0d
Jason Yee <jason.yee@datadoghq.com>
2019-09-03 14:04:42 -0700

    Delay the traffic generator

--- chaos-workshop/background.sh
+++ chaos-workshop/background.sh
@@ -3,7 +3,7 @@
 date > lastrun.txt
 
 # Clone the Memebook git repo (output to error.txt if something goes wrong)
-git clone https://github.com/jyee/memebook.git &> error.txt
+git clone https://github.com/jyee/memebook.git 2> error.txt
 
 # Move the Kubernetes bits out of the project, then delete the project (we don't need the code).
 mv memebook/kubernetes . && rm -r memebook
@@ -13,4 +13,7 @@ kubectl apply -f kubernetes/doggo.yaml
 kubectl apply -f kubernetes/lolcat.yaml
 kubectl apply -f kubernetes/redis-master.yaml
 kubectl apply -f kubernetes/redis-slave.yaml
+
+# Give the application some time to start, then generate traffic.
+sleep 30
 kubectl apply -f kubernetes/traffic_generator.yaml
