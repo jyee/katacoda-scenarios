33eddcde6cb0944592759a614934b04000329205
Jason Yee <jason.yee@datadoghq.com>
2019-09-03 12:11:31 -0700

    update setup scripts to use memebook

--- chaos-workshop/background.sh
+++ chaos-workshop/background.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 mkdir /chaos-workshop
 cd /chaos-workshop
-git clone https://github.com/jyee/distributed-tracing-with-apm-workshop.git .
+git clone https://github.com/jyee/memebook.git .
 date > lastrun.txt
-git fetch origin k8s-autodiscovery
-git checkout k8s-autodiscovery
 
-kubectl create secret generic postgres-user --from-literal=token=postgres
-kubectl create secret generic postgres-password --from-literal=token=postgres-password
-
-kubectl apply -f k8s-yaml-files/postgres-deploy.yaml
-kubectl apply -f k8s-yaml-files/redis-deploy.yaml
-kubectl apply -f k8s-yaml-files/pumps-service.yaml
-kubectl apply -f k8s-yaml-files/sensors-api.yaml
-kubectl apply -f k8s-yaml-files/node-api.yaml
-kubectl apply -f k8s-yaml-files/frontend-service.yaml
+kubectl apply -f kubernetes/memebook.yaml
+kubectl apply -f kubernetes/doggo.yaml
+kubectl apply -f kubernetes/lolcat.yaml
+kubectl apply -f kubernetes/redis-master.yaml
+kubectl apply -f kubernetes/redis-slave.yaml
+kubectl apply -f kubernetes/traffic_generator.yaml
