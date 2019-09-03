aed83d30c457c73c02d1ec011b5b3b2e72557584
Jason Yee <jason.yee@datadoghq.com>
2019-09-03 12:52:30 -0700

    simplify setup scripts

--- chaos-workshop/background.sh
+++ chaos-workshop/background.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-mkdir /chaos-workshop
-cd /chaos-workshop
-git clone https://github.com/jyee/memebook.git . &> error.txt
+# Ensure we're running a new build (can look at this to verify)
 date > lastrun.txt
+
+# Clone the Memebook git repo (output to error.txt if something goes wrong)
+git clone https://github.com/jyee/memebook.git &> error.txt
+
+# Move the Kubernetes bits out of the project, then delete the project (we don't need the code).
+mv memebook/kubernetes . && rm -r memebook
 
 kubectl apply -f kubernetes/memebook.yaml
 kubectl apply -f kubernetes/doggo.yaml
--- chaos-workshop/foreground.sh
+++ chaos-workshop/foreground.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source <(kubectl completion bash)
-echo [[HOST_IP]] >> /chaos-workshop/hostip
-cd /chaos-workshop
+echo [[HOST_IP]] >> hostip.txt
