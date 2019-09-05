23b563fe9dfce8a8a2701f058192cf8571131e0e
Jason Yee <jason.yee@datadoghq.com>
2019-09-05 15:29:05 -0700

    missing dollar

--- chaos-workshop/intro-foreground.sh
+++ chaos-workshop/intro-foreground.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "Setting up environments"
 
-if [ (hostname) == "node01" ]; then
+# Install tools on the kubernetes node.
+if [ $(hostname) == "node01" ]; then
   bash -c "apt-get update &&
     apt-get install golang-go stress-ng &&
     go get github.com/tylertreat/comcast"
 fi
-
-
