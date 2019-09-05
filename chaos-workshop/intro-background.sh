5596ead7fab4bd8d98a32128b2657539a3c14b10
Jason Yee <jason.yee@datadoghq.com>
2019-09-05 16:04:06 -0700

    update background

--- chaos-workshop/intro-background.sh
+++ chaos-workshop/intro-background.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # Clone the Memebook git repo (output to error.txt if something goes wrong)
-git clone https://github.com/jyee/memebook.git 2> error.txt
+git clone https://github.com/jyee/memebook.git &> git-messages.txt
 
 # Move the Kubernetes bits out of the project, then delete the project (we don't need the code).
+echo "Getting Memebook Kubernetes manifests" >> status.txt
 mv memebook/kubernetes . && rm -r memebook
 
+echo "Applying Kubernetes manifests" >> status.txt
 kubectl apply -f kubernetes/memebook.yaml
 kubectl apply -f kubernetes/doggo.yaml
 kubectl apply -f kubernetes/lolcat.yaml
 kubectl apply -f kubernetes/redis-master.yaml
 kubectl apply -f kubernetes/redis-slave.yaml
 
-ssh -oStrictHostKeyChecking=no node01 -c "apt-get update &&
-  apt-get -y install golang-go stress-ng"
+echo "Installing tools on host node (apt)" >> status.txt
+ssh -oStrictHostKeyChecking=no node01 "apt-get update && apt-get -y install golang-go stress-ng"
+
+#echo "Installing tools on host node (go)" >> status.txt
+#ssh -oStrictHostKeyChecking=no node01 "apt-get update && apt-get -y install golang-go stress-ng"
+
+echo "finished" >> status.txt
