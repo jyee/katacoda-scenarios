1370f7964a6d1dd774c6b7a00052cbb91c79cc78
Jason Yee <jason.yee@datadoghq.com>
2019-09-05 16:13:54 -0700

    update background

--- chaos-workshop/intro-background.sh
+++ chaos-workshop/intro-background.sh
@@ -7,15 +7,15 @@ git clone https://github.com/jyee/memebook.git &> git-messages.txt
 echo "Getting Memebook Kubernetes manifests" >> status.txt
 mv memebook/kubernetes . && rm -r memebook
 
-echo "Applying Kubernetes manifests" >> status.txt
-kubectl apply -f kubernetes/memebook.yaml
-kubectl apply -f kubernetes/doggo.yaml
-kubectl apply -f kubernetes/lolcat.yaml
-kubectl apply -f kubernetes/redis-master.yaml
-kubectl apply -f kubernetes/redis-slave.yaml
+#echo "Applying Kubernetes manifests" >> status.txt
+#kubectl apply -f kubernetes/memebook.yaml
+#kubectl apply -f kubernetes/doggo.yaml
+#kubectl apply -f kubernetes/lolcat.yaml
+#kubectl apply -f kubernetes/redis-master.yaml
+#kubectl apply -f kubernetes/redis-slave.yaml
 
-echo "Installing tools on host node (apt)" >> status.txt
-ssh -oStrictHostKeyChecking=no node01 "apt-get update && apt-get -y install golang-go stress-ng"
+echo "Node01 update apt" >> status.txt
+ssh -oStrictHostKeyChecking=no node01 "apt-get update" &> ssh.txt
 
 #echo "Installing tools on host node (go)" >> status.txt
 #ssh -oStrictHostKeyChecking=no node01 "apt-get update && apt-get -y install golang-go stress-ng"
