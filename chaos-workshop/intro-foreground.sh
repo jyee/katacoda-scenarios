eb4c779fbbe921eab7243f0fa00add8461a3e051
Jason Yee <jason.yee@datadoghq.com>
2019-09-05 15:23:42 -0700

    try to install on node term

--- chaos-workshop/intro-foreground.sh
+++ chaos-workshop/intro-foreground.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-source <(kubectl completion bash)
+echo "Setting up environments"
+
+if [ (hostname) == "node01" ]; then
+  bash -c "apt-get update &&
+    apt-get install golang-go stress-ng &&
+    go get github.com/tylertreat/comcast"
+fi
+
+
