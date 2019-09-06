0ffc48fc2ed2889bda25b1217589577c021eedb8
Jason Yee <jason.yee@datadoghq.com>
2019-09-06 09:29:57 -0700

    remove unused scripts from testing approaches

--- chaos-workshop/intro-foreground.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Setting up environments"
-
-# Install tools on the kubernetes node.
-if [ $(hostname) == "node01" ]; then
-  bash -c "apt-get update &&
-    apt-get install golang-go stress-ng &&
-    go get github.com/tylertreat/comcast"
-fi
